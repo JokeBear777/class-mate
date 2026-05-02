92ebb7ed18d4c231ec77b1780375a1e4b302eddb
Jokebear777 <korae9955@naver.com>
2026-05-03 00:37:54 +0900

    initial commit

---
+++ src/main/java/class_mate/class_mate/ClassMateApplication.java
@@ -0,0 +1,13 @@
+package class_mate.class_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/class_mate/class_mate/ClassMateApplicationTests.java
@@ -0,0 +1,13 @@
+package class_mate.class_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
